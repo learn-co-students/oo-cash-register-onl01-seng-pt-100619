b2f16a147ddbf5a2bb5b5fccb37a978ccec7fbf7
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-10 02:19:32 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  attr_accessor :total, :cash_register, :cash_register_with_discount
 
+
+
+  def initialize
+    @total = 0
+    @discount = 20
+
+  end
+
+  def discount
+    self.total
+
+
+  end
+
+  def total
+  end
+
+  def add_item
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
